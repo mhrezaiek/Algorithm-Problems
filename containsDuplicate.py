2fe1b5a9ef09caafcf176265aa3db960edafd1b9
Hinrich <mohamadh.rezaie@gmail.com>
2022-06-13 02:38:05 -0700

    containsDuplicate

---
+++ containsDuplicate.py
@@ -0,0 +1,12 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dic = {}
+        for i in nums:
+            if i in dic:
+                dic[i]+=1
+            else:
+                dic[i]=1
+        for i in dic.values():
+            if i > 1:
+                return True 
+        return False
