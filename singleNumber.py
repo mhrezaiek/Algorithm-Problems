04bf4e25480f4535fd63a7f2d99fafb481d15df6
Hinrich <mohamadh.rezaie@gmail.com>
2022-06-12 20:22:30 -0700

    singleNumber

---
+++ singleNumber.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        dic = {}
+        for i in range(len(nums)):
+            if nums[i] in dic:
+                dic[nums[i]] += 1
+            else:
+                dic[nums[i]]=1 
+            
+        for key, value in dic.items():
+            if value == 1:
+                return key
